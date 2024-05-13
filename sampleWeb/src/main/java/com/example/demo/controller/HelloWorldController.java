7083d8fd01f1decca5fbe2459eb08a5b9e8b3c0f
kimryu <test@tesutonoMacBook-Air.local>
2024-05-14 03:40:45 +0900

    second commit

---
+++ sampleWeb/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	 private String index() 
+	{
+	 return "index";
+			
+	  }
+	
+	@GetMapping("/get-message")
+	 private String getMessage(@RequestParam(required = false)String message,Model model) {
+		model.addAttribute("message",message);
+		return "get_message";
+	}
+	
+	 @GetMapping("/post-message")
+	  private String postMessage(){
+	    return "post_message";
+	  }
+	
+	@PostMapping("/confirm-message")
+	private String confirmMessage(@RequestParam String message,Model model) {
+		model.addAttribute("message",message);
+		return "confirm_message";
+	}
+	
+	}
